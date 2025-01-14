821db68a5d32f8ca534c05bd72b295b3705790bb
Robert Barrett <roborobbie123@gmail.com>
2025-01-14 12:57:56 -0500

    finalized article page layout

--- src/main/java/com/robb/personalblog/Controllers/ArticleController.java
+++ src/main/java/com/robb/personalblog/Controllers/ArticleController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -24,7 +26,19 @@ public class ArticleController {
     @RequestMapping("/article/{title}")
     public String articleSelect(@PathVariable String title, Model model) throws IOException {
         String article = articleService.getByTitle(title.replace(" ", ""));
-        model.addAttribute("article",article);
+
+        List<String> list = new ArrayList<>();
+        list = articleService.parseJson(article);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+
+
+
+
+        model.addAttribute("title", list.get(0));
+        model.addAttribute("content", list.get(1));
+        model.addAttribute("date", list.get(2));
 
         return "article";
     }
--- src/main/java/com/robb/personalblog/Service/ArticleService.java
+++ src/main/java/com/robb/personalblog/Service/ArticleService.java
@@ -1,5 +1,7 @@
 package com.robb.personalblog.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -9,18 +11,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ArticleService {
+
     public String getByTitle(String title) throws IOException {
 
         String file = "src/main/resources/articles/" + title + ".json";
         Path path = Paths.get(file);
 
-        if(!Files.exists(path)){
+        if (!Files.exists(path)) {
             return null;
         }
         String content = new String(Files.readAllBytes(path));
         return content;
+    }
+
+    public List<String> parseJson(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> list = new ArrayList<>();
+
+        try {
+            JsonNode node = objectMapper.readTree(json);
+
+            String title = node.get("title").asText();
+            String content = node.get("content").asText();
+            String date = node.get("date").asText();
+
+            list.add(title);
+            list.add(content);
+            list.add(date);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
